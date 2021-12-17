94f11e8b4c2ca39021bdc300307ad277e2c2867f
迷渡 <justjavac@gmail.com>
2021-12-17 15:42:02 +0800

    fix ci
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- libgit2/global_test.ts
+++ libgit2/global_test.ts
@@ -1,6 +1,6 @@
-import {  assert } from "https://deno.land/std/testing/asserts.ts";
+import { assert } from "https://deno.land/std/testing/asserts.ts";
 
-import {git_libgit2_init, git_libgit2_shutdown} from "./global.ts";
+import { git_libgit2_init, git_libgit2_shutdown } from "./global.ts";
 
 Deno.test("git_libgit2_init", (): void => {
   assert(git_libgit2_init() >= 0);
--- mod.ts
+++ mod.ts
@@ -1,18 +1,5 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write --allow-net --allow-ffi --unstable
 
-import * as libgit2 from "./libgit2/mod.ts";
-
-console.log("git_libgit2_init", libgit2.git_libgit2_init());
-console.log("git_libgit2_shutdown", libgit2.git_libgit2_shutdown());
-
-console.log("git_error_last", libgit2.git_error_last());
-
-const buf = new Uint8Array(8);
-const ptr = Deno.UnsafePointer.of(buf);
-const ptrView = new Deno.UnsafePointerView(ptr);
 console.log(
-  "git_repository_init",
-  libgit2.git_repository_init(ptr, "./tmp", false),
+  "⚠️ Work in progress, see: https://github.com/justjavac/deno_git/tree/main/examples",
 );
-
-libgit2.close();
